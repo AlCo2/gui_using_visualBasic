8a65082c230aae71631181ddb888f2d950b9bf2c
Alco2 <abdou.lahoub.wwe@gmail.com>
2023-03-08 18:45:01 +0100

    firstCommit

---
+++ login.vb
@@ -0,0 +1,27 @@
+﻿Public Class login
+
+    Private Sub Label1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Label1.Click
+
+    End Sub
+
+    Private Sub TextBox2_TextChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles TextBox2.TextChanged
+
+    End Sub
+
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
+        If TextBox1.Text = "abdou" And TextBox2.Text = "1234" Then
+            MsgBox("login sucessful")
+        Else
+            MsgBox("error in password or username...")
+        End If
+    End Sub
+
+    Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub Button2_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button2.Click
+        register.Show()
+        Me.Close()
+    End Sub
+End Class
---
+++ register.vb
@@ -0,0 +1,30 @@
+﻿Public Class register
+    Dim username As String
+    Dim password As String
+    Dim password1 As String
+
+    Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub Label1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Label1.Click
+
+    End Sub
+
+    Private Sub LinkLabel1_LinkClicked(ByVal sender As System.Object, ByVal e As System.Windows.Forms.LinkLabelLinkClickedEventArgs) Handles LinkLabel1.LinkClicked
+        login.Show()
+        Me.Close()
+    End Sub
+
+    Private Sub TextBox1_TextChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles TextBox1.TextChanged
+
+    End Sub
+
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
+        username = TextBox1.Text
+        password = TextBox2.Text
+        password1 = TextBox3.Text
+        login.Show()
+        Me.Close()
+    End Sub
+End Class
